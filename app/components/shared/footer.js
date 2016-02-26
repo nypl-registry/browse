ca6dbbc360849a7b9170e78c19fbe98776c54eec
Matt Miller <thisismattmiller@gmail.com>
2016-02-26 13:42:22 -0500

    webhook test

--- app/components/shared/footer.js
+++ app/components/shared/footer.js
@@ -14,6 +14,7 @@ const Footer = React.createClass({
         <div className="container">
           <div className="row header">
             <div className="twelve columns footer">
+            
                 <Data></Data>
             </div>
         </div>
